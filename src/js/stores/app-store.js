7502e05c793d100441f9e9395974fdcc36da1131
tree-waldo <leif.waldner@colorado.edu>
2016-02-06 18:26:22 -0700

    added stages, view now changes on stage

--- src/js/stores/app-store.js
+++ src/js/stores/app-store.js
@@ -5,13 +5,15 @@ var EventEmitter = require('events').EventEmitter;
 var CHANGE_EVENT = 'change';
 //var title_list = [];
 var char_list = [];
-var temp_list = ['Independent', 'Confident', 'Patient', 'Confident', 'Resourceful', 'Generous', 'Energetic', 'Ambitious', 'Optimistic', 'Clever', 'Encouraging', 'Humorous', 'Observant', 'Reliable', 'Accepting', 'Passionate'];
 
+var temp_list = ['Considerate', 'Dedicated', 'Patient', 'Honest', 'Sociable']
+var question_list = ['Do you value others\' well being above your own?', 'Are you willing to sacrifice your free-time to achieve your goals and aspirations', 'Does it bother you when you have to wait on people', 'If you found a wallet on the ground, would you return it as you found it?']
 
 for(var i=0; i<(temp_list.length); i++) {
     char_list.push({
-        'id': i,
-        'title': temp_list[i]
+        '
+        'title': temp_list[i],
+        'question': question_list[i]
     });
 }
 
@@ -19,7 +21,7 @@ var cart_items = [];
 
 function removeChar(index) {
     cart_items[index].inList = false;
-    cart_items.splice(index, 1);
+    cart_items.splice(index, 1); 
 }
 
 function addChar(item) {
@@ -28,6 +30,14 @@ function addChar(item) {
         cart_items.push(item);
     }
 }
+
+function addAnswer(item) {
+    if(!item.inList) {
+        item['inList'] = true;
+        answer_list.push(item);
+    }
+}
+
 var AppStore = assign(EventEmitter.prototype, {
     emitChange: function() {
         this.emit(CHANGE_EVENT)
@@ -56,6 +66,10 @@ var AppStore = assign(EventEmitter.prototype, {
 
             case "REMOVE_CHAR":
                 removeChar(payload.action.index);
+                break;
+            
+            case "ADD_ANSWER":
+                addAnswer(payload.action.item);
                 break;
         }
 
