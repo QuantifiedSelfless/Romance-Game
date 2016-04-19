2a32138f265ca9a28a5e3b280dff1bba7f460894
wannabeCitizen <doorsofskirpan@gmail.com>
2016-04-19 15:24:32 -0600

    still trying to fix this damn button

--- src/js/components/app-flipscreen.js
+++ src/js/components/app-flipscreen.js
@@ -10,7 +10,7 @@ var FlipScreen = React.createClass({
         return (
             <div className="flex flex-column center">
                 <img className="mx-auto mb3" src="./img/arrows-26-128.png"></img>
-                <button className="btn btn-primary mx-auto mt3" onClick={this.handler}>Continue</button>
+                <button className="btn btn-primary mt3" onClick={this.handler}>Continue</button>
             </div>
         )
     }
